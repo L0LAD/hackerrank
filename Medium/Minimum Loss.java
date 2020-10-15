e1ff72e0200e32b0de298b34a4a3811e43a55ff5
L0LAD <audrey.denoual@live.fr>
2020-10-15 17:29:15 +0200

    Minimum loss fonctionnel

--- Medium/Minimum Loss.java
+++ Medium/Minimum Loss.java
@@ -10,18 +10,22 @@ public class Solution {
 
     // Complete the minimumLoss function below.
     static int minimumLoss(long[] prices) {
-        long minList[] = new long[prices.length-1];
-
-        for (int i = 0; i < prices.length - 1; i++) {
-            long iMin = Long.MAX_VALUE;
-            for (int j = i+1; j < prices.length; j++) {
-                if (prices[i] > prices[j] && iMin > (prices[i]-prices[j]))
-                    iMin = prices[i]-prices[j];
-            }
-            minList[i] = iMin;
+        int n = prices.length;
+        HashMap<Long, Integer> pricesMap = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            pricesMap.put(prices[i], i);
         }
-        Arrays.sort(minList);
-        long result = minList[0];
+        Arrays.sort(prices);
+
+        long result = Long.MAX_VALUE;
+        for(int i = 0; i < n-1; i++){
+            if(prices[i+1] - prices[i] < result){
+                if(pricesMap.get(prices[i+1]) < pricesMap.get(prices[i]))
+                    result = prices[i+1] - prices[i];
+            }
+        }
+
         return (int)result;
     }
 
