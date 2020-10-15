70e1383496c0992cf30b6dfef7070620c0d3e081
L0LAD <audrey.denoual@live.fr>
2020-10-15 20:42:47 +0200

    Missing Numbers partiel

---
+++ Easy/Missing Numbers.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the missingNumbers function below.
+function missingNumbers(arr, brr) {
+    let result = [];
+    arr.sort();
+    brr.sort();
+    let a = 0;
+    for (let b = 0; b < brr.length; b++) {
+        if (arr[a] == brr[b])
+            a++;
+        else
+            result.push(brr[b]);
+    }
+
+    return result.sort().join(" ");
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const m = parseInt(readLine(), 10);
+
+    const brr = readLine().split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const result = missingNumbers(arr, brr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
