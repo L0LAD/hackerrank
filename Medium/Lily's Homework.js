a2f8c9fa7d4bc6f1293f72116f18ec91a62fbd87
L0LAD <audrey.denoual@live.fr>
2020-10-17 21:57:38 +0200

    Map to deal with timeout issue

--- Medium/Lily's Homework.js
+++ Medium/Lily's Homework.js
@@ -28,37 +28,54 @@ function readLine() {
 function lilysHomework(mixedArray) {
     var arr = mixedArray.slice();
     var brr = mixedArray.slice();
+    mixedArray.sort(function(a, b) {return a-b});
 
-    //Ascensing sort
-    mixedArray.sort(function(a,b) {return a-b});
-    var sortedArr = mixedArray.slice();
+    //Ascending sort
     var aResult = 0;
+    var sortedArr = mixedArray.slice();
+    var indexArr = [];
+    
     for (var i=0; i<arr.length; i++) {
+        indexArr[arr[i]] = i;
+    }
+    
+    for (var i=0; i<sortedArr.length; i++) {
         if (sortedArr[i] != arr[i]) {
             var numA = arr[i];
             var numB = sortedArr[i];
-            var rankB = arr.indexOf(numB);
+            var rankB = indexArr[sortedArr[i]];
+            indexArr[sortedArr[i]] = i;
+            indexArr[arr[i]] = rankB;
             arr[i] = numB;
             arr[rankB] = numA;
             aResult++;
         }
     }
-
+    
     //Descending sort
-    var desortedArr = mixedArray.slice().reverse();
     var bResult = 0;
-    for (var i=brr.length-1; i>=0; i--) {
+    var desortedArr = mixedArray.slice().reverse();
+    var indexBrr = [];
+    
+    for (var i=0; i<brr.length; i++) {
+        indexBrr[brr[i]] = i;
+    }
+
+    for (var i=0; i<desortedArr.length; i++) {
         if (desortedArr[i] != brr[i]) {
             var numA = brr[i];
             var numB = desortedArr[i];
-            var rankB = brr.indexOf(numB);
+            var rankB = indexBrr[desortedArr[i]];
+            indexBrr[desortedArr[i]] = i;
+            indexBrr[brr[i]] = rankB;
             brr[i] = numB;
             brr[rankB] = numA;
             bResult++;
         }
     }
-
+    
     return Math.min(aResult, bResult);
+
 }
 
 function main() {
